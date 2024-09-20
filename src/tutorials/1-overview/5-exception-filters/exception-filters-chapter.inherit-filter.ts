31c93461135f17678f1a478e38e3504fa0e1b505
AthiseenChaiyod <152632490+AthiseenChaiyod@users.noreply.github.com>
2024-09-20 09:48:56 +0700

    chapter completed: exception-filters

--- src/tutorials/1-overview/5-exception-filters/exception-filters-chapter.inherit-filter.ts
+++ src/tutorials/1-overview/5-exception-filters/exception-filters-chapter.inherit-filter.ts
@@ -1,8 +1,14 @@
 import { ArgumentsHost, Catch, HttpException } from '@nestjs/common';
-import { ExceptionFiltersChapterFilter } from './exception-filters-chapter.filter';
+import { BaseExceptionFilter } from '@nestjs/core';
 
+// บางครั้งเราก็อยากที่จะ extends BaseExceptionFilter มาใช้เฉย ๆ
 @Catch()
-export class ExceptionFiltersChapterInheritFilter extends ExceptionFiltersChapterFilter {
+// อันดับแรกก็ให้เรา extends BaseExceptionFilter มาก่อน
+export class ExceptionFiltersChapterInheritFilter extends BaseExceptionFilter {
+  // แล้วก็ให้เราประกาศ catch() เหมือนปกติ
+  // แต่ว่าโค้ดของ catch() เราจะต้องเรียก super.catch() ของ parent
+  // ประมาณว่าโค้ดนี้จะส่งโค้ดของตัวเองให้กับ parent ทำงานนั่นแหละ
+  // แล้วเราก็ปรับแต่งเพิ่มเติมเอาต่อด้านล่างของ super.catch() ได้เลย
   catch(exception: HttpException, host: ArgumentsHost) {
     super.catch(exception, host);
   }
